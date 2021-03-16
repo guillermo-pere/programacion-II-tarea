07992c4bde7902fe585b874e589cc02f05777871
Guillermo Perez <gp2933@gmail.com>
2021-03-15 19:12:46 -0700

    Programacion 2 trabajo 1

---
+++ src/main/java/ni/edu/uni/programacion/controllers/ConversorController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ni.edu.uni.programacion.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import ni.edu.uni.programacion.views.panels.PnlConversor;
+
+/**
+ *
+ * @author GABO
+ */
+public class ConversorController implements ActionListener{
+    private PnlConversor pnlconversor;
+    
+public ConversorController (PnlConversor pnlconversor){
+    this.pnlconversor= pnlconversor;
+    initComponent();
+}
+private void initComponent(){
+        pnlconversor.getBtnConv1().addActionListener(this);
+        pnlconversor.getBtnConv2().addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    double n1;
+    n1 = Double.parseDouble(pnlconversor.getTxtConv1().getText());
+    if(e.getActionCommand().equalsIgnoreCase("Cordoba a Dolar")){
+     pnlconversor.getTxtConv2().setText(String.valueOf(producto(n1,0.028)));
+    }
+    if(e.getActionCommand().equalsIgnoreCase("Dolar a Cordoba")){
+     pnlconversor.getTxtConv2().setText(String.valueOf(producto(n1,35.10)));
+    }
+    }
+    private double producto(double a, double b){
+        return (a*b);
+    }
+}
